cd1a990052e292fa726669d3653e65b2f4554e87
Tiana Husted <tiana.husted@gmail.com>
2020-06-15 19:43:25 -0700

    Comitting GNews API call to script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+console.log("hello");
+
+//GNews API Call
+let query = "genesis%20financial%20solutions";
+
+let token = "561ded44bea85bd1b3058e9aeb6484da";
+
+let queryURL = "https://gnews.io/api/v3/search?q=" + query + "&token=" + token;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
